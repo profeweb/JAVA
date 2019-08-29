e68d0f868e6ef278d8a8aa7fb05226a977a25615
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-08-29 20:50:32 +0200

    Ex 3

---
+++ U01 - FONAMENTS/ex03_Notes/src/ex03_notes/Ex03_Notes.java
@@ -0,0 +1,16 @@
+
+package ex03_notes;
+
+public class Ex03_Notes {
+
+    public static void main(String[] args) {
+        System.out.println("Notes de Matemàtiques.");
+        System.out.print("Nota del 1r Parcial:");
+        System.out.println(4.5);
+        System.out.print("Nota del 2n Parcial:");
+        System.out.println(7.5);
+        System.out.print("Nota Final:");
+        System.out.println(6);
+    }
+    
+}
