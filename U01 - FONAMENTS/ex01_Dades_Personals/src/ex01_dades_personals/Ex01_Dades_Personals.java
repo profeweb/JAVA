aa8e1f92922f300dc5cdefed81ac5e94800e9d12
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-08-29 20:46:21 +0200

    Ex 01

---
+++ U01 - FONAMENTS/ex01_Dades_Personals/src/ex01_dades_personals/Ex01_Dades_Personals.java
@@ -0,0 +1,13 @@
+
+package ex01_dades_personals;
+
+
+public class Ex01_Dades_Personals {
+
+
+    public static void main(String[] args) {
+        System.out.println("Jaume Sureda Adrover.");
+        System.out.println("12/06/1981.");
+    }
+    
+}
