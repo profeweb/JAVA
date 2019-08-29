8e7c4f7b22c903de313594342cc6ef78077db4b3
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-08-29 20:48:58 +0200

    Ex 2

---
+++ U01 - FONAMENTS/ex02_Dades_Personals_v2/src/ex02_dades_personals_v2/Ex02_Dades_Personals_v2.java
@@ -0,0 +1,13 @@
+
+package ex02_dades_personals_v2;
+
+public class Ex02_Dades_Personals_v2 {
+
+    public static void main(String[] args) {
+        System.out.print("Jaume ");
+        System.out.print("Sureda ");
+        System.out.println("Adrover.");
+        System.out.println("12/06/1981.");
+    }
+    
+}
